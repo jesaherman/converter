a252970b2956c50de0939db5ea59f26611bbb0f2
Eric Settels and Jesa Herman <jesa@>
2020-01-13 11:29:36 -0800

    add gallons, cups, and ounces

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
-var gallonsToLiters = funtion()
+//functions
+var gallonsToLiters = function(num1) {
+    return num1 * 3.785;
+
+}
+var cupsToMl = function(num1){
+    return num1 * 236.588;
+}
+
+var ozToG = function(num1){
+    return num1 * 28.35;
+}
+
+
+//variables
+//gallons
+var num1 = parseFloat(prompt("Enter gallons"));
+alert(gallonsToLiters(num1));
+//cups
+var num2 = parseFloat(prompt("Enter cups"));
+alert(cupsToMl(num2));
+//Oz
+var num3 = parseFloat(prompt("Enter Ounces"));
+alert(ozToG(num3));
+
+
+
+
+
+
+
+
+
+
