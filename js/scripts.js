a9e80438001e0ad7ef7c49da82c1bdace8640a2e
Eric Settels and Jesa Herman <eric@>
2020-01-13 11:37:35 -0800

    add tbls to ml

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,9 @@ var cupsToMl = function(num1){
 var ozToG = function(num1){
     return num1 * 28.35;
 }
+var tblsToMl = function(num1){
+    return num1 * 14.787;
+}
 
 
 //variables
@@ -22,6 +25,9 @@ alert(cupsToMl(num2));
 //Oz
 var num3 = parseFloat(prompt("Enter Ounces"));
 alert(ozToG(num3));
+var num4 = parseFloat(prompt("Enter tablespoons"));
+alert(`${tblsToMl(num4)} milliliters`);
+
 
 
 
